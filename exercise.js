34e58bdc0c65cde81ba0de57b9feb95a968726cb
Tiffany <twarre30@gmail.com>
2022-03-25 16:02:06 -0500

    solutions

--- exercise.js
+++ exercise.js
@@ -1,3 +1,37 @@
+function candySalesToObject(sales){
+    return {
+        item: sales[0],
+        price: + sales[1],
+        quantity: sales[2]
+    }
+}
+
+function salesDayToObject(date, sales){
+    return sales[date].reduce((a, b) => {
+        a.date = date
+        a.sales.push(candySalesToObject(b))
+        return a
+    }, {sales: []})
+        
+}
+
+function allSalesToArray(allSales){
+    return Object.keys(allSales)
+    .map(date => salesDayToObject(date, allSales));
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
 module.exports = {
     candySalesToObject,
     salesDayToObject,

b24629148e8dc68979764a04cf5524c9d900e77e
Kyle Coberly <kyle.coberly@gmail.com>
2022-03-25 08:51:50 -0600

    Clean up format

--- exercise.js
+++ exercise.js
@@ -1,34 +1,34 @@
 function candySalesToObject(candySalesArray){
-    return candySalesArray.reduce((object, item, index)=>{
-        switch(index){
-            case 0:
-                object.item = item;
-                break;
-            case 1:
-                object.price = item;
-                break;
-            case 2:
-                object.quantity = item;
-                break;
-        }
-        return object;
-    }, {});
+  return candySalesArray.reduce((object, item, index) => {
+    switch(index){
+      case 0:
+        object.item = item;
+        break;
+      case 1:
+        object.price = item;
+        break;
+      case 2:
+        object.quantity = item;
+        break;
+    }
+    return object;
+  }, {});
 }
 
 function salesDayToObject(date, allSales){
-    return allSales[date].reduce((object, day)=>{
-        object.date = date;
-        object.sales.push(candySalesToObject(day));
-        return object;
-    }, {sales: []});
+  return allSales[date].reduce((object, day)=> {
+    object.date = date;
+    object.sales.push(candySalesToObject(day));
+    return object;
+  }, {sales: []});
 }
 
 function allSalesToArray(allSales){
-    return Object.keys(allSales).map(date => salesDayToObject(date, allSales));
+  return Object.keys(allSales).map(date => salesDayToObject(date, allSales));
 }
 
 module.exports = {
-    candySalesToObject,
-    salesDayToObject,
-    allSalesToArray
+  candySalesToObject,
+  salesDayToObject,
+  allSalesToArray
 };
